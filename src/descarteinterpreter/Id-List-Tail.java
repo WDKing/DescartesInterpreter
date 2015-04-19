ef4ecb3d857db6791f35e5fa1fb3aa4609f3bdaf
AnsataMamoru <king.williamd.wdk@gmail.com>
2015-04-18 23:17:03 -0600

    Create Id-List-Tail.java

---
+++ src/descarteinterpreter/Id-List-Tail.java
@@ -0,0 +1,66 @@
+/**
+ * CS3210: Descartes Interpreter assignment
+ * @author Ben Boudra
+ * @author Shelley King
+ * @author William King
+ */
+
+package descarteinterpreter;
+
+/**
+ * Representation of a "Id-List-Tail" node in a parse tree for Descartes-2
+ */
+public class Id-List-Tail extends ParseTreeNode {
+    
+    public Id-List-Tail(int code) {
+        super(code);
+    }
+    
+    protected Id-List-Tail(int code, ParseTreeNode parent) {
+        super(code, parent);
+    }
+    
+    /**
+     * Add child nodes based on the current token and the grammar's rules.
+     * @param   token   the current token
+     */
+    public void populateChildren(TokenPair token) {
+        int tokenNum = token.getTokenNum();
+        
+        switch(tokenNum) {
+            case 0:     // PERIOD
+            case 1:     // SEMICOLON
+            case 4:     // ELSE
+            case 5:     // FI
+            case 9:     // REPEAT
+                        doRule22(token);
+                        break;
+            case 14:    // ,
+                        doRule21(token);
+                        break;
+            default:    throw new IllegalArgumentException();
+        }
+      }
+    
+    //  List of Rules to Complete
+
+    /**
+     * Add child nodes based on rule 21 in the grammar:
+     * "21.   id-list-tail : , ID id-list-tail"
+     * @param   token   the current token
+     */
+    private void doRule21(TokenPair token) {
+        addChild(14);
+        addChild(7);
+        addChild(43);
+    }
+    
+    /**
+     * Add child nodes based on rule 22 in the grammar:
+     * "22.   id-list-tail :"
+     * @param   token   the current token
+     */
+    private void doRule22(TokenPair token) {
+        // Purposely Left Empty
+    }
+}
