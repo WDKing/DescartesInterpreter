1999ce44ec2c280808258d914f16fe2f73d09e17
AnsataMamoru <king.williamd.wdk@gmail.com>
2015-04-19 00:23:02 -0600

    Create ArithExprNode.java

---
+++ src/descarteinterpreter/ArithExprNode.java
@@ -0,0 +1,52 @@
+/**
+ * CS3210: Descartes Interpreter assignment
+ * @author Ben Boudra
+ * @author Shelley King
+ * @author William King
+ */
+
+package descarteinterpreter;
+
+/**
+ * Representation of a "ArithExpr" node in a parse tree for Descartes-2
+ */
+public class ArithExprNode extends ParseTreeNode {
+    
+    public ArithExprNode(int code) {
+        super(code);
+    }
+    
+    protected ArithExprNode(int code, ParseTreeNode parent) {
+        super(code, parent);
+    }
+    
+    /**
+     * Add child nodes based on the current token and the grammar's rules.
+     * @param   token   the current token
+     */
+    public void populateChildren(TokenPair token) {
+        int tokenNum = token.getTokenNum();
+        
+        switch(tokenNum) {
+            case 7:     // ID
+            case 24:    // -
+            case 27:    // (
+            case 29:    // CONST
+                        doRule37(token);
+                        break;
+            default:    throw new IllegalArgumentException();
+        }
+      }
+    
+    //  List of Rules to Complete
+    
+    /**
+     * Add child nodes based on rule 37 in the grammar:
+     * "37.   arith-expr : term term-tail"
+     * @param   token   the current token
+     */
+    private void doRule37(TokenPair token) {
+        addChild(50);
+        addChild(51);
+    }
+}
