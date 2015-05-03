704071148da5e6db31dd55aed79f45e9c597a86a
benjamin <benjamin@gmail.com>
2015-05-03 16:27:48 -0600

    Version 2.0
    ProgNodeTest Created, but not completed

---
+++ test/descarteinterpreter/ProgNodeTest.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package descarteinterpreter;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author benjamin
+ */
+public class ProgNodeTest {
+	
+	public ProgNodeTest() {
+	}
+	
+	@BeforeClass
+	public static void setUpClass() {
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+	}
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void tearDown() {
+	}
+
+	/**
+	 * Test of buildTree method, of class ProgNode.
+	 */
+	@Test
+	public void testBuildTree() {
+		System.out.println("buildTree");
+		// TODO review the generated test code and remove the default call to fail.
+		
+		fail("The test case is a prototype.");
+	}
+
+	/**
+	 * Test of execute method, of class ProgNode.
+	 */
+	@Test
+	public void testExecute() {
+		System.out.println("execute");
+		// TODO review the generated test code and remove the default call to fail.
+		
+	}
+
+	/**
+	 * Test of populateChildren method, of class ProgNode.
+	 */
+	@Test
+	public void testPopulateChildren() {
+		System.out.println("populateChildren");
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
+
+	public void buildTestTree1()
+	{
+		
+	}
+}
