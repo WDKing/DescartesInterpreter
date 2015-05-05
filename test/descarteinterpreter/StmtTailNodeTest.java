5377f9840eff5a9e623da9d2690b63f75464c151
Benjamin <Benjamin@gmail.com>
2015-05-04 20:33:58 -0600

    v 2.1 Fixed error in factorTail Wrote tests for ProgNode,StmtListNode,StmtNode,StmtTailNode

--- test/descarteinterpreter/StmtTailNodeTest.java
+++ test/descarteinterpreter/StmtTailNodeTest.java
@@ -15,13 +15,14 @@ import static org.junit.Assert.*;
  * @author Benjamin
  */
 public class StmtTailNodeTest {
-	ProgNode rule1Tree; 
+	
+	ProgNode rule2Tree; 
 	ProgNode parent;
 	TestingTools testTools;
-
+	
 	@Before
 	public void setUp() {
-		rule1Tree = this.buildRule1Tree();
+		rule2Tree = this.buildRule2Tree();
 		parent = new ProgNode();
 		testTools = new TestingTools();
 	}
@@ -30,133 +31,25 @@ public class StmtTailNodeTest {
 	public void tearDown() {
 	}
 
-	/**
-	 * Test of populateChildren method, of class StmtTailNode.
-	 */
 	@Test
-	public void testPeriodCase() {
-		DescartesToken tok = new DescartesToken(".",0,-1);
-		StmtListNode stmt = new StmtListNode(31, this.parent, -1);
-		stmt.populateChildren(tok);
-		this.testTools.compareTrees(parent, rule1Tree);
-	}
-	/**
-	 * Test of populateChildren method, of class StmtTailNode.
-	 */
-	@Test
-	public void testSemiColoncase() {
-		DescartesToken tok = new DescartesToken(";",1,-1);
-		StmtListNode stmt = new StmtListNode(31, this.parent, -1);
-		stmt.populateChildren(tok);
-		this.testTools.compareTrees(parent, rule1Tree);
-	}
-
-	/**
-	 * Test of populateChildren method, of class StmtTailNode.
-	 */
-	@Test
-	public void testIfCase() {
-		DescartesToken tok = new DescartesToken("IF",2,-1);
-		StmtListNode stmt = new StmtListNode(31, this.parent, -1);
-		stmt.populateChildren(tok);
-		this.testTools.compareTrees(parent, rule1Tree);
-	}
-
-	/**
-	 * Test of populateChildren method, of class StmtTailNode.
-	 */
-	@Test
-	public void testElseCase() {
-		DescartesToken tok = new DescartesToken("ELSE",4,-1);
-		StmtListNode stmt = new StmtListNode(31, this.parent, -1);
-		stmt.populateChildren(tok);
-		this.testTools.compareTrees(parent, rule1Tree);
-	}
-
-	/**
-	 * Test of populateChildren method, of class StmtTailNode.
-	 */
-	@Test
-	public void testFiCase() {
-		DescartesToken tok = new DescartesToken("FI",5,-1);
-		StmtListNode stmt = new StmtListNode(31, this.parent, -1);
-		stmt.populateChildren(tok);
-		this.testTools.compareTrees(parent, rule1Tree);
-	}
-
-	/**
-	 * Test of populateChildren method, of class StmtTailNode.
-	 */
-	@Test
-	public void testLoopCase() {
-		DescartesToken tok = new DescartesToken("LOOP",6,-1);
-		StmtListNode stmt = new StmtListNode(31, this.parent, -1);
-		stmt.populateChildren(tok);
-		this.testTools.compareTrees(parent, rule1Tree);
-	}
-
-	/**
-	 * Test of populateChildren method, of class StmtTailNode.
-	 */
-	@Test
-	public void testIdCase() {
-		DescartesToken tok = new DescartesToken("SomeID",7,-1);
-		StmtListNode stmt = new StmtListNode(31, this.parent, -1);
-		stmt.populateChildren(tok);
-		this.testTools.compareTrees(parent, rule1Tree);
-	}
-
-	/**
-	 * Test of populateChildren method, of class StmtTailNode.
-	 */
-	@Test
-	public void testRepeatCase() {
-		DescartesToken tok = new DescartesToken("REPEAT",9,-1);
-		StmtListNode stmt = new StmtListNode(31, this.parent, -1);
-		stmt.populateChildren(tok);
-		this.testTools.compareTrees(parent, rule1Tree);
-	}
-
-	/**
-	 * Test of populateChildren method, of class StmtTailNode.
-	 */
-	@Test
-	public void testBreakCase() {
-		DescartesToken tok = new DescartesToken("BREAK",10,-1);
-		StmtListNode stmt = new StmtListNode(31, this.parent, -1);
-		stmt.populateChildren(tok);
-		this.testTools.compareTrees(parent, rule1Tree);
-	}
-
-	/**
-	 * Test of populateChildren method, of class StmtTailNode.
-	 */
-	@Test
-	public void testPrintCase() {
-		DescartesToken tok = new DescartesToken("PRINT",12,-1);
-		StmtListNode stmt = new StmtListNode(31, this.parent, -1);
-		stmt.populateChildren(tok);
-		this.testTools.compareTrees(parent, rule1Tree);
-	}
-
-	/**
-	 * Test of populateChildren method, of class StmtTailNode.
-	 */
-	@Test
-	public void testReadCase() {
-		DescartesToken tok = new DescartesToken("READ",13,-1);
-		StmtListNode stmt = new StmtListNode(31, this.parent, -1);
-		stmt.populateChildren(tok);
-		this.testTools.compareTrees(parent, rule1Tree);
-	}
-
-	public ProgNode buildRule1Tree()
+	public void testSemicolonCase()
 	{
-		ProgNode prog = new ProgNode();
-		prog.addChild(31, -1);
-		StmtListNode stmt = (StmtListNode) prog.getChildAt(0);
-		stmt.addChild(32, -1);
-		stmt.addChild(33, -1);
-		return prog;
+		parent.addChild(33, -1);
+		DescartesToken tok = new DescartesToken(";",1,-1);
+		StmtTailNode stmt = (StmtTailNode) parent.getChildAt(0);
+		stmt.populateChildren(tok);
+		assertTrue(this.testTools.compareTrees(parent, rule2Tree));
 	}
+
+	public ProgNode buildRule2Tree()
+	{
+		ProgNode parent = new ProgNode();
+		parent.addChild(33, -1);
+		StmtTailNode stmtTail = (StmtTailNode) parent.getChildAt(0);
+        	stmtTail.addChild(1, -1);
+        	stmtTail.addChild(32, -1);
+        	stmtTail.addChild(33, -1);
+		return parent;
+	}
+	
 }
